d866bc35823e36a12c1ce6176efc6a0c44cca7fe
steelbridge-io <chris@steelbridge.io>
2021-01-17 19:00:57 -0800

    Updated default template

--- single.php
+++ single.php
@@ -11,7 +11,7 @@ get_header();
 ?>
 </div>
 
-<div id="primary" class="content-area">
+<div id="primary" class="content-area default-single">
 		<main id="main" class="site-main">
 
 		<?php
--- template-parts/content.php
+++ template-parts/content.php
@@ -6,32 +6,43 @@
  *
  * @package The_Fly_Shop
  */
-
+  global $post;
 ?>
+
+<div id="default-blog-hero-image" class="inner">
+  
+  <?php
+    if ( has_post_thumbnail() ) {
+    $jumbotronImage_default = get_the_post_thumbnail_url($post->ID, 'full');
+    $post_title = get_the_title($post->ID);
+    $post_permalink = get_the_permalink($post->ID);
+    $default_logo = get_theme_mod('default_page_logo');
+  ?>
+  
+  <div id="outfitters-jumbotron" class="outfitters default-template jumbotron">
+    <img class="img-responsive default-template-img outfitters" src="<?php echo $jumbotronImage_default ?>" alt="">
+    <div class="container">
+      
+      <dl class="landing-hd">
+        <?php if($default_logo !== '') { ?>
+          <dd class="dd-1"><img src="<?php echo $default_logo; ?>" class="img-responsive-logo" alt="" title=""></dd>
+        <?php } ?>
+        <dd class="dd-2"><h2 class="logo-tel text-center outfitters"><?php echo get_the_title(); ?></h2></dd>
+        <dd class="dd-4"><h3 class="logo-tel text-center outfitters"><a href="tel:18006693474">800 &bull; 669 &bull; 3474</a></h3></dd>
+      </dl>
+    
+    </div>
+  </div>
+  
+  <?php } ?>
+  
+</div>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   
   <div class="article-content row">
 
-<?php if ( has_post_thumbnail() ) {
-  global $post;
-  $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
-  $post_title = get_the_title($post->ID);
-  $post_permalink = get_the_permalink($post->ID);
-  
-  ?>
-  
-  <div class="col-md-4">
-    <a href="<?php echo $post_permalink; ?>" title="<?php echo $post_title; ?>">
-    <img class="img-responsive" src="<?php echo $featured_img_url ?>" alt="">
-    </a>
-    
-  </div>
-  
-		
-<?php } ?>
-
-	<div id="content-template-part-content" class="col-md-8">
+	<div id="content-template-part-content" class="container">
 
 		<div id="default-entry-styles" class="entry-content content-part">
           
@@ -40,11 +51,7 @@
           </a>
           <p><b>Published:&nbsp;<?php echo get_the_date( 'F j, Y' ); ?></b></p>
 			<?php
-				the_excerpt( sprintf(
-					/* translators: %s: Name of current post. */
-					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'the-fly-shop' ), array( 'span' => array( 'class' => array() ) ) ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				) );
+				the_content();
 
 				wp_link_pages( array(
 					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'the-fly-shop' ),
